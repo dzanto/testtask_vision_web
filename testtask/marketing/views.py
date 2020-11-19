5a43571c1661c527697a0787fd684cb891fa2a0b
dzanto <dzanto@gmail.com>
2020-11-19 19:54:47 +0300

    add pic

--- testtask/marketing/views.py
+++ testtask/marketing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from.models import Card
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    card1 = get_object_or_404(Card, pk=1)
+    return render(request, 'index.html', {"card1": card1})
